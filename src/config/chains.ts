100a2d275ffadc510f88f53ab380ca90fa3c9006
Christian Eichinger <christianeichinger@MacBook-Pro-von-Christian.local>
2024-03-08 14:49:10 +0100

    add ZEN logo

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -33,6 +33,8 @@ const ChainLogos = {
   [chains.arb1]: '/images/networks/arb.svg',
   [chains.avax]: '/images/networks/avax.svg',
   [chains.sep]: '/images/networks/sep.png',
+  [chains.Gobi]: '/images/networks/horizen-gobi.svg',
+  [chains.horizen]: '/images/networks/horizen-eon.svg',
 }
 
 export const getChainLogo = (chainId: string) => {
